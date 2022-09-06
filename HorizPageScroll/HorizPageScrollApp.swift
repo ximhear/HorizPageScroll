56a540bf587fef59fbd0058f6b0e46b4832c8781
gzonelee <gzonelee@gmail.com>
2022-09-06 15:49:07 +0900

    Initial Commit

---
+++ HorizPageScroll/HorizPageScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizPageScrollApp.swift
+//  HorizPageScroll
+//
+//  Created by gzonelee on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct HorizPageScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
