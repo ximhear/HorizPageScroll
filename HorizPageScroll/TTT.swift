fc2866a4eced67dfcc798fa52d5f60c6deebbe11
gzonelee <gzonelee@gmail.com>
2022-09-07 02:12:04 +0900

    .

---
+++ HorizPageScroll/TTT.swift
@@ -0,0 +1,20 @@
+//
+//  TTT.swift
+//  HorizPageScroll
+//
+//  Created by gzonelee on 2022/09/06.
+//
+
+import SwiftUI
+
+struct TTT: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TTT_Previews: PreviewProvider {
+    static var previews: some View {
+        TTT()
+    }
+}
